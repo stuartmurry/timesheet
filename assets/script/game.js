c70dcb853a710c1b728fc6c23b3fb8fcf4497c4f
stuartmurry <stuartmurry93@gmail.com>
2018-03-13 11:36:30 -0600

    Added Delete

--- assets/script/game.js
+++ assets/script/game.js
@@ -14,67 +14,144 @@ $(document).ready(function() {
   function Timesheet(database) {
     return {
       database: database,
-      getEmployees : function() {
-        return this.database.ref('Employees');
+      getEmployees: function() {
+        return this.database.ref("Employees");
+      },
+      computeMonthsWorked: function(rate) {
+        var startDate = moment.unix(rate);
+        var dateNow = moment();
+
+        var m = dateNow.diff(startDate, "months");
+        console.log("Months Worked");
+        console.log(m);
+
+        return m;
+      },
+      computeMonthlyBilled: function(rate) {
+        console.log("Months Worked");
+        console.log(rate);
       },
       submit: function() {
+        var _this = this;
         var employee = {
           Employee: $("#EmployeeName").val(),
           Role: $("#Role").val(),
           StartDate: moment($("#StartDate").val()).unix(),
           MonthlyRate: $("#MonthlyRate").val()
         };
-
-        console.log('inserting employee');
-        console.log(employee);
-
-        this.getEmployees()
-          .push(employee)
-          .then(
-            function(data) {
-              // Clear Forms
-              $("#EmployeeName").val("");
-              $("#Role").val("");
-              $("#StartDate").val("");
-              $("#MonthlyRate").val("");
-            },
-            function(error) {
-              alert("Unable to save");
-            }
-          );
+        if ($("#ID").val()) {
+          // Has ID VAL
+        } else {
+          // Create New
+          this.getEmployees()
+            .push(employee)
+            .then(
+              function(data) {
+                _this.clear();
+              },
+              function(error) {
+                alert("Unable to save");
+              }
+            );
+        }
 
         // console.log(employee);
       },
-      insert: function(ts) {
-        
+      remove: function(id) {
+        console.log("removing id");
+        // Removes from list
+        $("#" + id).remove();
+      },
+      delete: function(id) {
+        // removes from database
+        var _this = this;
+        console.log(this.getEmployees());
+        _this.database
+          .ref("/Employees/" + id)
+          .remove()
+          .then(
+            function(d) {
+              _this.clear();
+            },
+            function(err) {
+              alert(err);
+            }
+          );
+      },
+      create: function() {
+        this.clear();
+        $("#Employee").removeClass("hidden");
+      },
+      clear: function() {
+        // Clear Forms
+        $("#ID").val("");
+        $("#EmployeeName").val("");
+        $("#Role").val("");
+        $("#StartDate").val("");
+        $("#MonthlyRate").val("");
+        $("#Employee").addClass("hidden");
+      },
+      insert: function(key, ts) {
+        var empl = {
+          id: key.trim(),
+          Employee: ts.Employee,
+          Role: ts.Role,
+          StartDate: moment.unix(ts.StartDate).format("MM/DD/YYYY"),
+          MonthlyRate: ts.MonthlyRate,
+          MonthsWorked: this.computeMonthsWorked(ts.StartDate),
+          MonthlyBilled: this.computeMonthlyBilled(ts.StartDate)
+        };
+
         console.log(ts);
         var arr = [];
-        arr.push('<tr>');
-        arr.push('<td>' + ts.Employee + '</td>');
-        arr.push('<td>' + ts.Role + '</td>');
-        arr.push('<td>' + moment.unix(ts.StartDate).format('MM/DD/YYYY') + '</td>');
-        arr.push('<td>' + ts.MonthlyRate + '</td>');
+        arr.push('<tr id="' + empl.id + '">');
+        arr.push("<td>" + empl.Employee + "</td>");
+        arr.push("<td>" + empl.Role + "</td>");
+        arr.push("<td>" + empl.StartDate + "</td>");
+        arr.push("<td>" + empl.MonthsWorked + "</td>");
+        arr.push("<td>" + empl.MonthlyRate + "</td>");
+        arr.push("<td>" + empl.MonthlyBilled + "</td>");
+        arr.push(
+          '<td><button id="delete' +
+            empl.id +
+            '" type="button" class="btn btn-danger">Delete</button></td>'
+        );
 
-        arr.push('</tr>');
-        
-        $("#tbody").append(arr.join(''));
-        
-        // $("#EmployeeName").val(ts.EmployeeBane);
-        // $("#Role").val();
-        // $("#StartDate").val();
-        // $("#MonthlyRate").val();
+        arr.push("</tr>");
+
+        $("#tbody").append(arr.join(""));
+        $("#" + empl.id).on("click", function(e) {
+          $("#Id").val(empl.id);
+          $("#EmployeeName").val(empl.Employee);
+          $("#Role").val(empl.Role);
+          $("#StartDate").val(empl.StartDate);
+          $("#MonthlyRate").val(empl.MonthlyRate);
+          $("#Employee").removeClass("hidden");
+        });
+
+        $("#delete" + empl.id).on("click", function(e) {
+          timesheet.delete(empl.id);
+        });
       }
     };
   }
 
-  var timesheet = new Timesheet(firebase.database());
+  timesheet = new Timesheet(firebase.database());
 
   timesheet.getEmployees().on("child_added", function(snapshot) {
-    timesheet.insert(snapshot.val());
+    timesheet.insert(snapshot.key, snapshot.val());
+  });
+
+  timesheet.getEmployees().on("child_removed", function(snapshot) {
+    timesheet.remove(snapshot.key);
   });
 
   $("#submit").on("click", function(evt) {
     timesheet.submit();
+  });
+
+  $("#AddEmployee").on("click", function(evt) {
+    timesheet.create();
   });
 
   $("#StartDate").datepicker({});
